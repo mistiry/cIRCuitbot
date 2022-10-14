18b67d839e793ce3b9a19d1caaee1a14a0fe0e58
mistiry <php.circuit.bot@gmail.com>
2022-10-13 21:35:38 -0500

    fixing setMode lib file

--- lib/setMode.php
+++ lib/setMode.php
@@ -1,34 +1,17 @@
 <?php
-function setMode($direction,$mode,$appliesto) {
+function setMode($direction,$mode,$user) {
     global $dbconnection;
     global $socket;
     global $ircdata;
     global $config;
 
-    //Confirm requestor has ADMIN rights to run
-    $requestor = $ircdata['userhostname'];
-    $requestorBotFlags = getBotFlags($requestor);
-    if($requestorBotFlags == "A") {
-        //user is permitted
-        if($appliesto == $config['channel']) {
-            $command = "MODE $appliesto $direction$mode";
-        } elseif($appliesto == "") {
-            $channel = $config['channel'];
-            $user = $ircdata['usernickname'];
-            $command = "MODE $channel $direction$mode $user";
-        } else {
-            $appliesto = mysqli_real_escape_string($dbconnection,$appliesto);
-            $query = "SELECT id FROM known_users WHERE nick_aliases LIKE '%$appliesto%'";
-            $result = mysqli_query($dbconnection,$query);
-            if(mysqli_num_rows($result) > 0) {
-                $channel = $config['channel'];
-                $command = "MODE $channel $direction$mode $appliesto";
-            }
-        }
-        logEntry("Admin user '".$ircdata['usernickname']."' requested '".$direction."".$mode."' for user '".$sppliesto."'");
-        fputs($socket,"$command\n");
-    } else {
-        logEntry("Denied setMode for non-admin user '".$ircdata['usernickname']."@".$ircdata['userhostname']."'");
+    $user = mysqli_real_escape_string($dbconnection,$user);
+    $query = "SELECT id FROM known_users WHERE nick_aliases LIKE '%$user%'";
+    $result = mysqli_query($dbconnection,$query);
+    if(mysqli_num_rows($result) > 0) {
+        $command = "MODE ".$config['channel']." ".$direction."".$mode." ".$user."";
     }
+    logEntry("setMode called to '".$direction."".$mode." ".$user."'");
+    fputs($socket,"$command\n");
     return true;
 }

2f6eb3214a9c4164561e6528c63b864f321438f5
mistiry <php.circuit.bot@gmail.com>
2024-03-08 11:40:04 -0600

    updating sendPRIVMSG to handle messages of any length properly

--- lib/sendPRIVMSG.php
+++ lib/sendPRIVMSG.php
@@ -1,6 +1,36 @@
 <?php
 function sendPRIVMSG($location,$message) {
     global $socket;
-    fputs($socket, "PRIVMSG ".$location." :".$message."\n");
-    return;
+
+    $maxLength = 375;
+
+    //If $message is 375 or less:
+    if (strlen($message) <= $maxLength) {
+        fputs($socket, "PRIVMSG ".$location." :".$message."\n");
+    }
+    
+    // Find the nearest whitespace before the max length
+    $lastSpace = strrpos(substr($message, 0, $maxLength), ' ');
+    
+    // // If there's no whitespace, just split at the max length
+    // if ($lastSpace === false) {
+    //     return [substr($message, 0, $maxLength), substr($message, $maxLength)];
+    // }
+    
+    // Split the message at the nearest whitespace
+    $firstPart = substr($message, 0, $lastSpace);
+    $secondPart = substr($message, $lastSpace + 1);
+    
+    // Recursively split the second part
+    $remainingParts = splitString($secondPart, $maxLength);
+    
+    // Combine the first part with the recursively split parts
+    array_unshift($remainingParts, $firstPart);
+
+    //Foreach message in array
+    foreach($remainingParts as $message) {
+        fputs($socket, "PRIVMSG ".$location." :".$message."\n");
+    }
+
+    return true;
 }

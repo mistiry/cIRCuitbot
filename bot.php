5476d906039bfed32f81f5881a39acc00d9706b4
mistiry <php.circuit.bot@gmail.com>
2024-07-13 00:08:42 -0400

    fixing encoding to match the db

--- bot.php
+++ bot.php
@@ -42,7 +42,7 @@ if(!$dbconnection) {
     die("Unable to connect to database; error: " . mysqli_connect_error());
 }
 if(!mysqli_set_charset($dbconnection, "utf8mb4")) {
-    die("Unable to set database character set to UTF-8mb4);
+    die("Unable to set database character set to UTF-8mb4");
 }
 
 

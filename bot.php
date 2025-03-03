c2cfb24a88552e7650f808d726326f113d039ffc
mistiry <php.circuit.bot@gmail.com>
2025-03-02 20:24:41 -0500

    more bugfixes

--- bot.php
+++ bot.php
@@ -187,7 +187,7 @@ while(1) {
 
                 //Run the query
                 if(mysqli_query($dbconnection,$query)) {
-                    continue 2;
+                    continue;
                 } else {
                     logEntry("Unable to update user record for '".$ircdata['usernickname']."@".$ircdata['userhostname']."'");
                 }

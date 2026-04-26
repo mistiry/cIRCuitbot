c41675dc77ee77c77f38cb9f300a23c1e17177a9
mistiry <mistiry@gmail.com>
2026-04-26 15:40:01 -0400

    Fix PHP warning: replace continue with break in known_users switch
    continue inside a switch targets the switch itself in PHP 7+, which
    is identical to break. The warning was harmless but misleading.
    No behavior change.

--- bot.php
+++ bot.php
@@ -186,7 +186,7 @@ while(1) {
 
                 //Run the query
                 if(mysqli_query($dbconnection,$query)) {
-                    continue;
+                    break;
                 } else {
                     logEntry("Unable to update user record for '".$ircdata['usernickname']."@".$ircdata['userhostname']."'", 'ERROR');
                 }
